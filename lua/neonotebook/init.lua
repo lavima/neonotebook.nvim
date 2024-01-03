ad19b0aa5eeeb2ebc2636a22d1d688d155a1a8ae
Lars Magnusson <lavima@gmail.com>
2024-01-03 22:40:28 +0100

    update

--- lua/neonotebook/init.lua
+++ lua/neonotebook/init.lua
@@ -5,10 +5,22 @@ local language_extensions = {
 }
 
 local outputs_extension = '.ipyout'
+local outputs_text_extension = '.ipyout.out'
 
-local shellescape = vim.fn.shellescape
+local remove_undo = function()
+  -- In order to make :undo a no-op immediately after the buffer is read, we
+  -- need to do this dance with 'undolevels'.  Actually discarding the undo
+  -- history requires performing a change after setting 'undolevels' to -1 and,
+  -- luckily, we have one we need to do (delete the extra line from the :r
+  -- command)
+  -- (Comment straight from goerz/jupytext.vim)
+  local levels = vim.o.undolevels
+  vim.o.undolevels = -1
+  vim.api.nvim_command "silent delete"
+  vim.o.undolevels = levels
+end
 
-local convert_to_script = function(filepath)
+local convert_notebook = function(filepath)
   local metadata = vim.json.decode(io.open(filepath, "r"):read "a")["metadata"]
 
   local language = metadata.kernelspec.language
@@ -17,15 +29,7 @@ local convert_to_script = function(filepath)
   local script_filepath = vim.fn.fnamemodify(filepath, ':r') .. extension
   local outputs_filepath = vim.fn.fnamemodify(filepath, ':r') .. outputs_extension
 
-  local convert_script = require('neonotebook.utils').get_lua_dir() .. '../../python/ipynb/convert.py'
-  local command = 'python3 ' .. shellescape(convert_script) .. ' ' .. shellescape(filepath) .. 
-    ' --output ' .. shellescape(script_filepath) .. ' --cell_outputs ' .. outputs_filepath
-
-  local output = vim.fn.system(command)
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_err_writeln(command .. ": " .. output .. ": " .. vim.v.shell_error)
-    return
-  end
+  require('neonotebook.convert').convert_notebook(filepath, script_filepath, outputs_filepath)
 
   if vim.fn.filereadable(script_filepath) then
     local script_content = vim.fn.readfile(script_filepath)
@@ -41,32 +45,61 @@ local convert_to_script = function(filepath)
     pattern = "<buffer>",
     group = "neonotebook",
     callback = function(ev)
-      write_to_ipynb(ev.match, output_extension)
+      convert_script(ev.match, output_extension)
     end,
   })
 
-  vim.api.nvim_command("setlocal fenc=utf-8 ft=" .. language)
+  --vim.api.nvim_command("setlocal fenc=utf-8 ft=" .. language)
+  vim.opt_local.fenc='utf-8'
+  vim.opt_local.ft=language
 
-  -- In order to make :undo a no-op immediately after the buffer is read, we
-  -- need to do this dance with 'undolevels'.  Actually discarding the undo
-  -- history requires performing a change after setting 'undolevels' to -1 and,
-  -- luckily, we have one we need to do (delete the extra line from the :r
-  -- command)
-  -- (Comment straight from goerz/jupytext.vim)
-  local levels = vim.o.undolevels
-  vim.o.undolevels = -1
-  vim.api.nvim_command "silent delete"
-  vim.o.undolevels = levels
+
+  remove_undo()
 
   -- First time we enter the buffer redraw. Don't know why but jupytext.vim was
   -- doing it. Apply Chesterton's fence principle.
-  vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "<buffer>",
-    group = "neonotebook",
-    once = true,
-    command = "redraw",
-  })
+  --vim.api.nvim_create_autocmd("BufEnter", {
+  --  pattern = "<buffer>",
+  --  group = "neonotebook",
+  --  once = true,
+  --  command = "redraw",
+  --})
 end
+
+local convert_outputs = function(filepath)
+  local out_text_filepath = vim.fn.fnamemodify(filepath, ':r') .. outputs_text_extension
+
+  require('neonotebook.convert').convert_outputs(filepath, out_text_filepath)
+
+  if vim.fn.filereadable(out_text_filepath) then
+    local text_content = vim.fn.readfile(out_text_filepath)
+
+    -- Replace the buffer content with the script content
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, text_content)
+  else
+    error "Couldn't find out text file."
+    return
+  end
+
+  vim.bo.modifiable = false
+  vim.bo.buftype = 'nowrite'
+  vim.opt_local.fenc = 'utf-8'
+  vim.opt_local.ft = 'nn_out'
+  vim.opt_local.cursorline = false
+
+
+  remove_undo()
+
+  -- First time we enter the buffer redraw. Don't know why but jupytext.vim was
+  -- doing it. Apply Chesterton's fence principle.
+  --vim.api.nvim_create_autocmd("BufEnter", {
+  --  pattern = "<buffer>",
+  --  group = "neonotebook",
+  --  once = true,
+  --  command = "redraw",
+  --})
+end
+
 
 M.setup = function(config)
   vim.validate(config,'table')
@@ -76,7 +109,15 @@ M.setup = function(config)
     pattern = { "*.ipynb" },
     group = "neonotebook",
     callback = function(ev)
-      convert_to_script(ev.match)
+      convert_notebook(ev.match)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("BufReadCmd", {
+    pattern = { "*.ipyout" },
+    group = "neonotebook",
+    callback = function(ev)
+      convert_outputs(ev.match)
     end,
   })
 end
